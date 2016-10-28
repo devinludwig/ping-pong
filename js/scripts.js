9b0011e54cbf14208c488ca0f12ebc06b099cb9e
Devin Ludwig <devinludwig@gmail.com>
2016-10-28 09:36:42 -0700

    add header font and background coloralong with list background color and text colors and create img folder in project directory.

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,11 @@ var count = function(number) {
   array = [];
   for (var index = 1; index <= number; index ++) {
     if (index % 15 === 0) {
-      array.push("<li>ping-pong</li>");
+      array.push("<li class='pingpong'>ping-pong</li>");
     } else if (index % 3 === 0) {
-      array.push("<li>ping</li>");
+      array.push("<li class='ping'>ping</li>");
     } else if (index % 5 === 0) {
-      array.push("<li>pong</li>");
+      array.push("<li class='pong'>pong</li>");
     } else {
       array.push("<li>" + index + "</li>");
     }
@@ -20,7 +20,7 @@ var count = function(number) {
 $(function() {
   $('form').submit(function(event) {
     event.preventDefault();
-    $('.result').empty();
+    $('.result').empty().show();
     var input = $('input').val();
     $('.result').append(count(input));
   });
