bfe7ed7f3136e39c6b93bd260e9721a05b02caad
Devin Ludwig <devinludwig@gmail.com>
2016-10-28 09:00:01 -0700

    add functioning business logic to produce desired specs and return output to user interface.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
+var input = parseInt('input');
+var array = [];
+
+var count = function(number) {
+  array = [];
+  for (var index = 1; index <= number; index ++) {
+    if (index % 15 === 0) {
+      array.push("ping-pong");
+    } else if (index % 3 === 0) {
+      array.push("ping");
+    } else if (index % 5 === 0) {
+      array.push("pong");
+    } else {
+      array.push(index);
+    }
+  }
+  return(array);
+}
+
 $(function() {
   $('form').submit(function(event) {
     event.preventDefault();
